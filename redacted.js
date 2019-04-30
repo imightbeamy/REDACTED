966f090300b691e6301e31063d3986ec534d4a19
Setsun <setsun@setsun.io>
2019-04-30 13:29:42 -0400

    replace dat text

--- redacted.js
+++ redacted.js
@@ -1,1 +1,28 @@
-console.log("hi")
+const superlatives = [
+  'best',
+  'awesome',
+  'sickest',
+  'nicest',
+  'coolest',
+];
+
+const textElements = [
+  ...document.getElementsByTagName('h1'),
+  ...document.getElementsByTagName('h2'),
+  ...document.getElementsByTagName('h3'),
+  ...document.getElementsByTagName('h4'),
+  ...document.getElementsByTagName('h5'),
+  ...document.getElementsByTagName('h6'),
+  ...document.getElementsByTagName('p'),
+  ...document.getElementsByTagName('span'),
+];
+
+textElements.forEach((element) => {
+  const { innerText } = element;
+  const regex = new RegExp(`(${superlatives.join('|')})`, 'g');
+  const replaceContent = innerText.replace(regex, (matched) => {
+    return `<span class="highlight">${matched}</span>`
+  });
+
+  element.innerHTML = replaceContent;
+});
